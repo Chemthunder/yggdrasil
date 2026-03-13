9bd4cad9207763ca8bd27a0bf22c52831067ab2d
chemthunder <chemthunderisme@gmail.com>
2026-03-13 18:25:32 -0400

    wgat was i smoking this is heat

--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+
+namespace test {
+    let testInit = theseus.createProject("Test");
+
+    let testKey = new StringKey("wawa");
+
+    let str = testKey.unpack();
+    testInit.log(str);
+}
