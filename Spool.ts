6572e10f5df7e40651ed6222e1b755754556bae4
chemthunder <chemthunderisme@gmail.com>
2026-03-17 11:14:10 -0400

    Updated Geode and whatnot

--- Spool.ts
+++ Spool.ts
@@ -81,3 +81,45 @@ class KeySpool {
         }
     }
 }
+
+class BranchSpool {
+    _attachedSprite: Sprite;
+    _content: Branch;
+
+    constructor(content: Branch) {
+        this._content = content;
+    }
+
+    private get sprite(): Sprite { return this._attachedSprite; }
+    private get content(): Branch { return this._content; }
+
+    private set sprite(input: Sprite) {
+        this._attachedSprite = input;
+    }
+
+    addTo(spriteToAdd: Sprite) {
+        this.sprite = spriteToAdd;
+    }
+
+    removeFrom() {
+        this.sprite = null;
+    }
+
+    getSprite(): Sprite {
+        return this.sprite;
+    }
+
+    hasSpool(sprite: Sprite): boolean {
+        return this.getSprite() == sprite;
+    }
+
+    unravel(): any {
+        if (this.getSprite() != null) {
+            try {
+                return this.content;
+            } catch {
+                Yggdrasil.stop("Cannot unravel Spool: " + this);
+            }
+        }
+    }
+}
